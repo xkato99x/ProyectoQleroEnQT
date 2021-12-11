f3462e66a498d00b487095e66519cd04244ac085
xkato99x <73802717+xkato99x@users.noreply.github.com>
2021-12-11 14:00:22 -0600

    Add files via upload

--- Proyecto.asm
+++ Proyecto.asm
@@ -3,6 +3,7 @@
     out ddrd,r16 
     ldi r16,0xff		    ;R16 carga todos los pines como salida
     out ddrb,r16
+    ldi r23,0
     
 inicio:
     ldi r20,0
@@ -63,7 +64,6 @@ azul:
     call delay
     call delay
     call apagaB
-    call delay
     call memoria
     
 verde:
@@ -72,7 +72,6 @@ verde:
     call delay
     call delay
     call apagaB
-    call delay
     call memoria
     
 rojo:
@@ -81,7 +80,6 @@ rojo:
     call delay
     call delay
     call apagaB
-    call delay
     call memoria
     
 amarillo:
@@ -90,7 +88,6 @@ amarillo:
     call delay
     call delay
     call apagaB
-    call delay
     call memoria
   
 apagaB:
@@ -122,88 +119,71 @@ memoria:
     ret
     
 memUno:
-    /*in r25,portc
-    ldi r17,9
-    out portc,r17
-    call delay
-    out portc,r25*/
     mov r16,r31
     inc r16
     sts 0x100,r16
     rjmp start
     
 memDos:
-    /*in r25,portc
-    ldi r17,8
-    out portc,r17
-    call delay
-    out portc,r25*/
     mov r16,r31
     inc r16
     sts 0x101,r16
     rjmp start
     
 memTres:
-    /*in r25,portc
-    ldi r17,7
-    out portc,r17
-    call delay
-    out portc,r25*/
     mov r16,r31
     inc r16
     sts 0x102,r16
     rjmp start
     
 memCuatro:
-    /*in r25,portc
-    ldi r17,6
-    out portc,r17
-    call delay
-    out portc,r25*/
     mov r16,r31
     inc r16
     sts 0x103,r16
     rjmp start
     
 memCinco:
-    /*in r25,portc
-    ldi r17,0
-    out portc,r17
-    call delay
-    out portc,r25*/
     mov r16,r31
     inc r16
     sts 0x104,r16
     rjmp start
     
 convertidor:
+    call pseudo
     cpi r22,1
     breq conAzul
     
+    call pseudo
     cpi r22,2
     breq conVerde
     
+    call pseudo
     cpi r22,3
     breq conRojo
     
+    call pseudo
     cpi r22,4
     breq conAmarillo
 
     ret
     
 conAzul:
+    call pseudo
     ldi r22,0b00000001
     ret
     
 conVerde:
+    call pseudo
     ldi r22,0b00000010
     ret
     
 conRojo:
+    call pseudo
     ldi r22,0b00000100
     ret
     
 conAmarillo:
+    call pseudo
     ldi r22,0b00001000
     ret    
     
@@ -211,6 +191,7 @@ atajoRan:
     rjmp random
     
 recuerda:
+    call pseudo
     // Prender lo que salió la 1ra vez
     lds r22,0x100
     cpi r22,0
@@ -218,12 +199,11 @@ recuerda:
     call convertidor
     out portb,r22
     call delay
-    out portb,r22
-    call delay
     ldi r22,0
     out portb,r22
     call delay
     
+    call pseudo
     // Prender lo que salió la 2da vez
     lds r22,0x101
     cpi r22,0
@@ -235,6 +215,7 @@ recuerda:
     out portb,r22
     call delay
     
+    call pseudo
     // Prender lo que salió la 3ra vez
     lds r22,0x102
     cpi r22,0
@@ -242,12 +223,11 @@ recuerda:
     call convertidor
     out portb,r22
     call delay
-    out portb,r22
-    call delay
     ldi r22,0
     out portb,r22
     call delay
     
+    call pseudo
     //Prender lo que salió la 4ta vez
     lds r22,0x103
     cpi r22,0
@@ -255,19 +235,16 @@ recuerda:
     call convertidor
     out portb,r22
     call delay
-    out portb,r22
-    call delay
     ldi r22,0
     out portb,r22
     call delay
     
+    call pseudo
     //Prender lo que salió la 5ta vez
     lds r22,0x104
     cpi r22,0
     breq atajoRand
     call convertidor
-    out portb,r22
-    call delay
     out portb,r22
     call delay
     ldi r22,0
@@ -304,11 +281,13 @@ start:
     ldi r20,1
     in r19,pind
     cpi r19,0b00001001
-    breq cuatroUno
+    breq cuartoUno
+    call pseudo
+    
     ldi r20,2
     in r19,pind
     cpi r19,0b00001010
-    breq cuatroDos
+    breq cuartoDos
     
     call miniDelay
     call apagaD
@@ -322,11 +301,13 @@ start:
     ldi r20,3
     in r19,pind
     cpi r19,0b00010001
-    breq cincoUno
+    breq quintoUno
+    call pseudo
+    
     ldi r20,4
     in r19,pind
     cpi r19,0b00010010
-    breq cincoDos
+    breq quintoDos
     
     call miniDelay
     call apagaD
@@ -339,32 +320,55 @@ reinicio:
     rjmp sueltaReinicio
     rjmp reinicio
     
-cuatroUno:	    ;Presionamos el botón del led AZUL
+cuartoUno:	    ;Prendemos el led AZUL
     call pseudo
-    sbis pind,0
-    rjmp compara
-    rjmp cuatroUno
+    ldi r16,0b00000001
+    out portb,r16
+    rjmp pressCuartoUno
     
-cuatroDos:	    ;Presionamos el botón del led VERDE
+cuartoDos:	    ;Prendemos el led VERDE
     call pseudo
-    sbis pind,1
-    rjmp compara
-    rjmp cuatroDos
+    ldi r16,0b00000010
+    out portb,r16
+    rjmp pressCuartoDos
 
-cincoUno:		    ;Presionamos el botón del led ROJO
+quintoUno:		    ;Prendemos el led ROJO
+    call pseudo
+    ldi r16,0b00000100
+    out portb,r16
+    rjmp pressQuintoUno
+    
+quintoDos:	    	    ;Prendemos el led AMARILLO
+    call pseudo
+    ldi r16,0b00001000
+    out portb,r16
+    rjmp pressQuintoDos
+    
+pressCuartoUno:	    ;Presionamos el botón del led AZUL
     call pseudo
     sbis pind,0
     rjmp compara
-    rjmp cincoUno
+    rjmp cuartoUno
     
-cincoDos:	    	    ;Presionamos el botón del led AMARILLO
+pressCuartoDos:	    ;Presionamos el botón del led VERDE
     call pseudo
     sbis pind,1
     rjmp compara
-    rjmp cincoDos
+    rjmp cuartoDos
+
+pressQuintoUno:		    ;Presionamos el botón del led ROJO
+    call pseudo
+    sbis pind,0
+    rjmp compara
+    rjmp quintoUno
+    
+pressQuintoDos:	    	    ;Presionamos el botón del led AMARILLO
+    call pseudo
+    sbis pind,1
+    rjmp compara
+    rjmp quintoDos
     
 sueltaReinicio:
-    call pseudo
     ldi r16,0
     out portb,r16
     ldi r16,0b00000001
@@ -398,6 +402,12 @@ sueltaReinicio:
     rjmp inicio
 
 compara:
+    call pseudo
+    call miniDelay
+    call miniDelay
+    ldi r16,0
+    out portb,r16
+    
     cpi r21,0
     breq unPunto
     
@@ -408,11 +418,27 @@ compara:
     breq tresPuntos
     
     cpi r21,3
-    breq cuatroPuntos
+    breq atajoCuatroPuntos
     
     cpi r21,4
-    breq cincoPuntos
+    breq atajoCincoPuntos
     
+    rjmp start
+ 
+atajoCuatroPuntos:
+    call pseudo
+    rjmp cuatroPuntos
+    
+atajoCincoPuntos:
+    call pseudo
+    rjmp cincoPuntos
+    
+atajoErroneo:
+    call pseudo
+    rjmp erroneo
+    
+atajoStart:
+    call pseudo
     rjmp start
     
 unPunto:
@@ -420,50 +446,182 @@ unPunto:
     ldi r21,1
     lds r16,0x100
     cp r16,r20
-    brne erroneo
+    brne atajoErroneo
     call correcto
     ret
     
 dosPuntos:
     call pseudo
-    ldi r21,2
-    lds r16,0x101
-    cp r16,r20
-    brne erroneo
-    call correcto
+    cpi r23,0
+    breq dosUno
+    cpi r23,1
+    breq dosDos
+    dosUno:
+	call pseudo
+	ldi r23,1
+	lds r16,0x100
+	cp r16,r20
+	breq atajoStart
+	rjmp erroneo
+    
+    dosDos:
+	call pseudo
+	ldi r21,2
+	lds r16,0x101
+	cp r16,r20
+	brne atajoErroneo
+	call correcto
+	
     ret
     
 tresPuntos:
     call pseudo
-    ldi r21,3
-    lds r16,0x102
-    cp r16,r20
-    brne erroneo
-    call correcto
+    cpi r23,0
+    breq tresUno
+    cpi r23,1
+    breq tresDos
+    cpi r23,2
+    breq tresTres
+    
+    tresUno:
+	call pseudo
+	ldi r23,1
+	lds r16,0x100
+	cp r16,r20
+	breq atajoStart
+	rjmp erroneo
+
+    tresDos:
+	call pseudo
+	ldi r23,2
+	lds r16,0x101
+	cp r16,r20
+	breq atajoStart
+	rjmp erroneo
+	
+    tresTres:
+	call pseudo
+	ldi r21,3
+	lds r16,0x102
+	cp r16,r20
+	brne atajoErroneooo
+	call correcto
+	
     ret
     
 cuatroPuntos:
     call pseudo
-    ldi r21,4
-    lds r16,0x103
-    cp r16,r20
-    brne erroneo
-    call correcto
+    cpi r23,0
+    breq cuatroUno
+    cpi r23,1
+    breq cuatroDos
+    cpi r23,2
+    breq cuatroTres
+    cpi r23,3
+    breq cuatroCuatro
+    
+    cuatroUno:
+	call pseudo
+	ldi r23,1
+	lds r16,0x100
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+
+    cuatroDos:
+	call pseudo
+	ldi r23,2
+	lds r16,0x101
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+	
+    cuatroTres:
+	call pseudo
+	ldi r23,3
+	lds r16,0x102
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+    
+    cuatroCuatro:
+	call pseudo
+	ldi r21,4
+	lds r16,0x103
+	cp r16,r20
+	brne atajoErroneooo
+	call correcto
+	
     ret
+    
+atajoStartt:
+    call pseudo
+    rjmp start
+
+atajoErroneooo:
+    call pseudo
+    rjmp erroneo
     
 cincoPuntos:		;5 Puntos, fin del juego :DDDD
     call pseudo
-    in r16,portc
-    inc r16
-    out portc,r16
-    lds r16,0x104
-    cp r16,r20
-    brne erroneo
-    call fin
+    cpi r23,0
+    breq cincoUno
+    cpi r23,1
+    breq cincoDos
+    cpi r23,2
+    breq cincoTres
+    cpi r23,3
+    breq cincoCuatro
+    cpi r23,4
+    breq cincoCinco
+    
+    cincoUno:
+	call pseudo
+	ldi r23,1
+	lds r16,0x100
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+
+    cincoDos:
+	call pseudo
+	ldi r23,2
+	lds r16,0x101
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+	
+    cincoTres:
+	call pseudo
+	ldi r23,3
+	lds r16,0x102
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+    
+    cincoCuatro:
+	call pseudo
+	ldi r23,4
+	lds r16,0x103
+	cp r16,r20
+	breq atajoStartt
+	rjmp erroneo
+    
+    cincoCinco:
+	call pseudo
+	in r16,portc
+	inc r16
+	out portc,r16
+	lds r16,0x104
+	cp r16,r20
+	brne erroneo
+	call fin
+	
     ret
     
 correcto:
     call pseudo
+    ldi r23,0
     in r16,portc
     inc r16
     out portc,r16
