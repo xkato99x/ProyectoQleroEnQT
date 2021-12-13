ac5b19790329b389613b0b68d68be18e38bc98ae
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-12-13 12:49:57 -0600

    Add files via upload

--- prueba2/prueba2.ino
+++ prueba2/prueba2.ino
@@ -1,7 +1,7 @@
-int greenDelay = 2500;
-int orangeDelay = 1300;
+int greenDelay = 3500;
+int orangeDelay = 1500;
 int redDelay = 2000;
-int vueltaMil = 1200;
+int vueltaMil = 2000;
 
 
 // semaforo1
@@ -19,6 +19,7 @@ int r3Pin = 10;
 int o3Pin = 9;
 int g3Pin = 8;
 
+int inf1 = 7;
 
 unsigned long greenMillis = 0;
 unsigned long orangeMillis = 0;
@@ -54,6 +55,7 @@ void setup() {
   pinMode(g3Pin, OUTPUT);
 
   pinMode(A0, INPUT);
+  pinMode(inf1, INPUT);
 
   //Inciamos el semaforo con verde en vuelta
   digitalWrite(g1Pin, HIGH);
@@ -62,17 +64,22 @@ void setup() {
   digitalWrite(r2Pin, HIGH);
   digitalWrite(r3Pin, HIGH);
   Serial.begin(9600);
+  Serial.println(" vuelta");
 }
 
 int i = 0;
 unsigned long tiempoActual = millis();
+int cont = 0;
 
 void loop() {
-    tiempoActual = millis();
+
+  tiempoActual = millis();
 
   if (Serial.available()) {
 
+    //Error
     if (Serial.read() == 'e') {
+      digitalWrite(vuelta, LOW);
       digitalWrite(g1Pin, LOW);
       digitalWrite(g2Pin, LOW);
       digitalWrite(g3Pin, LOW);
@@ -98,13 +105,22 @@ void loop() {
     }
   }
 
-  /*if (digitalRead(A0) == HIGH) {
+  //Se presiono el boton
+  if (digitalRead(A0) == HIGH) {
     while (digitalRead(A0) == HIGH) {}
-    Serial.println("Funciona");
-    }*/
+    Serial.println('y');
+  }
+  //Si pasa un carro se envia mensaje
+  if (digitalRead(inf1) == HIGH) {
+    while (digitalRead(inf1) == HIGH) {}
+    cont++;
+    String cad = String(cont);
+    Serial.println(" " + cad);
+  }
 
   if (tiempoActual - vueltaMillis >= vueltaMil
       && vueltaStatus == true && red2Status == true) {
+    Serial.println(" v1");
     greenMillis = millis();
     digitalWrite(vuelta, LOW);
     digitalWrite(r2Pin, LOW);
@@ -114,8 +130,9 @@ void loop() {
 
   }
 
-  if (tiempoActual - greenMillis >= greenDelay && green1Status == true && red2Status == true) {
-    Serial.println("Entra a verde");
+  if (tiempoActual - greenMillis >= greenDelay
+      && green1Status == true && red2Status == true) {
+
     orangeMillis = millis();
     digitalWrite(g1Pin, LOW);
     digitalWrite(g2Pin, LOW);
@@ -124,10 +141,11 @@ void loop() {
     digitalWrite(o1Pin, HIGH);
     digitalWrite(o2Pin, HIGH);
     orange1Status = true;
+
   }
 
   if (tiempoActual - orangeMillis >= orangeDelay && orange1Status == true) {
-    Serial.println("Entra a amarillo");
+
     redMillis = millis();
     digitalWrite(o1Pin, LOW);
     digitalWrite(o2Pin, LOW);
@@ -143,10 +161,12 @@ void loop() {
     digitalWrite(g3Pin, HIGH);
     green2Status = true;
     green2Millis = millis();
+    Serial.println(" v2");
   }
 
-  if (tiempoActual - green2Millis >= greenDelay && green2Status == true) {
-    Serial.println("Entra a otro semaforo");
+  if (tiempoActual - green2Millis >= greenDelay
+      && green2Status == true) {
+
     orange2Millis = millis();
     digitalWrite(g3Pin, LOW);
     green2Status = false;
@@ -155,7 +175,8 @@ void loop() {
     orange2Status = true;
   }
 
-  if (tiempoActual - orange2Millis >= orangeDelay && orange2Status == true) {
+  if (tiempoActual - orange2Millis >= orangeDelay
+      && orange2Status == true) {
     red2Millis = millis();
     digitalWrite(o3Pin, LOW);
     orange2Status = false;
@@ -164,7 +185,9 @@ void loop() {
     red2Status = true;
   }
 
-  if (tiempoActual - redMillis >= redDelay && red1Status == true && red2Status == true) {
+  if (tiempoActual - redMillis >= redDelay
+      && red1Status == true && red2Status == true) {
+    Serial.println(" vuelta");
     vueltaMillis = millis();
     digitalWrite(r1Pin, LOW);
     red1Status = false;
@@ -177,7 +200,7 @@ void loop() {
 }
 
 void reiniciar() {
-  
+
   tiempoActual = 0;
   greenMillis = 0;
   orangeMillis = 0;
@@ -197,10 +220,10 @@ void reiniciar() {
   green2Status = false;
   orange2Status = false;
   red2Status = true;
-  
+
   digitalWrite(vuelta, HIGH);
   digitalWrite(g1Pin, HIGH);
-  
+
   digitalWrite(r2Pin, HIGH);
   digitalWrite(r3Pin, HIGH);
 }
