09f4706283a01ce67118bcd605531c97afe3ba6b
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-12-14 17:55:25 -0600

    Add files via upload

--- prueba2/prueba2.ino
+++ prueba2/prueba2.ino
@@ -1,7 +1,7 @@
-int greenDelay = 3500;
-int orangeDelay = 1500;
-int redDelay = 2000;
-int vueltaMil = 2000;
+int greenDelay = 1000;
+int orangeDelay = 500;
+int redDelay = 950;
+int vueltaMil = 950;
 
 
 // semaforo1
@@ -20,6 +20,7 @@ int o3Pin = 9;
 int g3Pin = 8;
 
 int inf1 = 7;
+int inf2 = 6;
 
 unsigned long greenMillis = 0;
 unsigned long orangeMillis = 0;
@@ -55,7 +56,11 @@ void setup() {
   pinMode(g3Pin, OUTPUT);
 
   pinMode(A0, INPUT);
+  
+  //Sensores infrarrojos
   pinMode(inf1, INPUT);
+  pinMode(inf2, INPUT);
+  pinMode(A1, INPUT);
 
   //Inciamos el semaforo con verde en vuelta
   digitalWrite(g1Pin, HIGH);
@@ -64,12 +69,12 @@ void setup() {
   digitalWrite(r2Pin, HIGH);
   digitalWrite(r3Pin, HIGH);
   Serial.begin(9600);
-  Serial.println(" vuelta");
+  Serial.println(" vuel");
 }
 
 int i = 0;
 unsigned long tiempoActual = millis();
-int cont = 0;
+int cont1 = 0;
 
 void loop() {
 
@@ -79,6 +84,7 @@ void loop() {
 
     //Error
     if (Serial.read() == 'e') {
+
       digitalWrite(vuelta, LOW);
       digitalWrite(g1Pin, LOW);
       digitalWrite(g2Pin, LOW);
@@ -97,29 +103,48 @@ void loop() {
         digitalWrite(o2Pin, !digitalRead(o2Pin));
         digitalWrite(r3Pin, !digitalRead(r3Pin));
         digitalWrite(o3Pin, !digitalRead(o3Pin));
-        delay(500);
+        delay(50);
         i++;
       }
+
       reiniciar();
-
     }
-  }
 
-  //Se presiono el boton
+  }//Termina de captar
+
+  //Se presiono el boton para cerrrar comunicacion
   if (digitalRead(A0) == HIGH) {
     while (digitalRead(A0) == HIGH) {}
     Serial.println('y');
   }
+
   //Si pasa un carro se envia mensaje
   if (digitalRead(inf1) == HIGH) {
+
     while (digitalRead(inf1) == HIGH) {}
-    cont++;
-    String cad = String(cont);
-    Serial.println(" " + cad);
-  }
+    cont1++;
+    char cad1 = char(cont1);
+    Serial.println(' ' + cad1);
+
+  }/*  if (digitalRead(inf2) == HIGH) {
+
+    while (digitalRead(inf2) == HIGH) {}
+    cont1++;
+    char cad2 = char(cont1);
+    Serial.println(' ' + cad2);
+
+  }  if (digitalRead(A1) == HIGH) {
+
+    while (digitalRead(A1) == HIGH) {}
+    cont1++;
+    char cad3 = char(cont1);
+    Serial.println(' ' + cad3);
+
+  }*/
 
   if (tiempoActual - vueltaMillis >= vueltaMil
       && vueltaStatus == true && red2Status == true) {
+
     Serial.println(" v1");
     greenMillis = millis();
     digitalWrite(vuelta, LOW);
@@ -128,10 +153,8 @@ void loop() {
     vueltaStatus = false;
     green1Status = true;
 
-  }
-
-  if (tiempoActual - greenMillis >= greenDelay
-      && green1Status == true && red2Status == true) {
+  } else if (tiempoActual - greenMillis >= greenDelay
+             && green1Status == true && red2Status == true) {
 
     orangeMillis = millis();
     digitalWrite(g1Pin, LOW);
@@ -142,9 +165,8 @@ void loop() {
     digitalWrite(o2Pin, HIGH);
     orange1Status = true;
 
-  }
-
-  if (tiempoActual - orangeMillis >= orangeDelay && orange1Status == true) {
+  } else if (tiempoActual - orangeMillis >= orangeDelay
+             && orange1Status == true) {
 
     redMillis = millis();
     digitalWrite(o1Pin, LOW);
@@ -162,10 +184,9 @@ void loop() {
     green2Status = true;
     green2Millis = millis();
     Serial.println(" v2");
-  }
 
-  if (tiempoActual - green2Millis >= greenDelay
-      && green2Status == true) {
+  } else if (tiempoActual - green2Millis >= greenDelay
+             && green2Status == true) {
 
     orange2Millis = millis();
     digitalWrite(g3Pin, LOW);
@@ -173,21 +194,21 @@ void loop() {
 
     digitalWrite(o3Pin, HIGH);
     orange2Status = true;
-  }
 
-  if (tiempoActual - orange2Millis >= orangeDelay
-      && orange2Status == true) {
+  } else if (tiempoActual - orange2Millis >= orangeDelay
+             && orange2Status == true) {
+
     red2Millis = millis();
     digitalWrite(o3Pin, LOW);
     orange2Status = false;
 
     digitalWrite(r3Pin, HIGH);
     red2Status = true;
-  }
 
-  if (tiempoActual - redMillis >= redDelay
-      && red1Status == true && red2Status == true) {
-    Serial.println(" vuelta");
+  } else if (tiempoActual - redMillis >= redDelay
+             && red1Status == true && red2Status == true) {
+
+    Serial.println(" vuel");
     vueltaMillis = millis();
     digitalWrite(r1Pin, LOW);
     red1Status = false;
