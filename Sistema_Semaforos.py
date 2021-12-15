81882b4eeaedf76acd74529fe3a28634e26be7ef
xkato99x <73802717+xkato99x@users.noreply.github.com>
2021-12-14 19:30:34 -0600

    Add files via upload

--- Sistema_Semaforos.py
+++ Sistema_Semaforos.py
@@ -1,13 +1,16 @@
 from tkinter import *
 import time as tm
 import threading as th
-import serial, random
+import serial
+import random
 
 arduino = serial.Serial('/dev/ttyS1', 9600)
-tm.sleep(0.8)
+tm.sleep(1)
 
-global con
-con = 0
+#global con
+con1 = 0
+con2 = 0
+con3 = 0
 
 def recibe():
     while True:
@@ -24,7 +27,7 @@ def recibe():
                 print("Cadena:", cad[:esp])
                 print("Long:", len(cad[:esp]))
                 imprime(cad[:esp])
-            tm.sleep(0.3)
+            tm.sleep(0.5)
             
 def envia():
     arduino.write('e'.encode())
@@ -35,7 +38,7 @@ def inicia():
     t.start()
 
 def imprime(sem):
-    
+    global con1, con2, con3
     if sem == 'v1':
         
         verde1.config(text="Semaforo1")
@@ -43,6 +46,16 @@ def imprime(sem):
         
         rojo1.config(text="Semaforo3")
         rojo2.config(text="")
+        
+        con = random.randint(0,con1)
+        con1 -= con
+        print(" Pasaron ",con," carros de los que habia en con1")
+        carros1.config(text=str(con1))
+        
+        con = random.randint(0,con2)
+        con2 -= con
+        print(" Pasaron ",con," carros de los que habia en con2")
+        carros2.config(text=str(con2))
         
     elif sem == "vuel":
         
@@ -59,32 +72,46 @@ def imprime(sem):
         rojo1.config(text="Semaforo1")
         rojo2.config(text="Semaforo2")
         
-    else:
-        carros1.config(text=sem)
-        num = random.radint(0,3)
-        print(num)
+        con = random.randint(0,con3)
+        con3 -= con
+        print(" Pasaron ",con," carros de los que habia en con3")
+        carros3.config(text=str(con3))
         
-
+    elif sem == "c1":
+        con = random.randint(1,5)
+        con1 += con
+        print(" Random:",con)
+        carros1.config(text=str(con1))
+    elif sem == "c2":
+        con = random.randint(1,5)
+        con2 += con
+        print("  Random:",con2)
+        carros2.config(text=str(con2))
+    elif sem == "c3":
+        con = random.randint(1,5)
+        con3 += con
+        print("   Random:",con3)
+        carros3.config(text=str(con3))
+    
 def cierra():
     arduino.close()
     print("Adios!")
     raiz.destroy()
-        
-        
-
+    
+    
 raiz = Tk()
 raiz.title("Control de tráfico")
 raiz.resizable(True, False)
 raiz.geometry("650x200")
+
+cerrar = Button(raiz, text="Cierra", command=cierra)
+cerrar.place(x=0, y=0)
 
 bot = Button(raiz, text="Inicia", command=inicia)
 bot.place(x=30, y=50)
 
 bot2 = Button(raiz, text="Error", command=envia)
 bot2.place(x=30, y=80)
-
-cerrar = Button(raiz, text="Cierra", command=cierra)
-cerrar.place(x=0, y=0)
 
 #Colores de los semaforos
 lab1 = Label(raiz, text="Semaforos en verde", background='lightgreen',
@@ -111,13 +138,13 @@ lab1.place(x=265, y=145)
 lab1 = Label(raiz, text="Semaforo 3", border=1)
 lab1.place(x=385, y=145)
 
-carros1 = Label(raiz, text="3")
+carros1 = Label(raiz, text="5")
 carros1.place(x=180, y=165)
 
-carros2 = Label(raiz, text="3")
+carros2 = Label(raiz, text="5")
 carros2.place(x=305, y=165)
 
-carros3 = Label(raiz, text="3")
+carros3 = Label(raiz, text="5")
 carros3.place(x=420, y=165)
 
 
