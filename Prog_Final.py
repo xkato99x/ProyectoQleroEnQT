0fc403ce7f639e6fb8363a44fe48c526fe887bee
Eliud Ayala <73802657+Eliud20@users.noreply.github.com>
2021-12-16 13:15:09 -0600

    Add files via upload

--- Prog_Final.py
+++ Prog_Final.py
@@ -8,12 +8,17 @@ arduino = serial.Serial('/dev/ttyS2', 9600)
 tm.sleep(2)
 
 #global con
+con = 0
 con1 = 0
 con2 = 0
 con3 = 0
+conr = 0
+conr1 = 0
+conr2 = 0
+tot = 0
 
 def recibe():
-    
+    global tot
     while True:
         
         if arduino.inWaiting == 0:
@@ -23,6 +28,7 @@ def recibe():
             
             if arduino.read() == b'y':
                 print("Cerrando comunicacion, termine el programa...")
+                total()
                 arduino.close()
                 break
             
@@ -38,7 +44,15 @@ def recibe():
        
 def envia():
     arduino.write('e'.encode())
+    print("Entrando en estado de error")
+    for i in range(5):
+        tm.sleep(0.5)
 
+
+def total():
+    global tot
+    tot_pas.config(text=str(tot))
+    
 
 def inicia():
     print("Iniciando...")
@@ -47,12 +61,20 @@ def inicia():
 
 
 def imprime(sem):
-    global con1, con2, con3
-    
+    global con, conr, conr1, conr2, con1, con2, con3, tot
     if sem == 'v1':
         
         verde1.config(text="Semaforo1")
+        conr1 = random.randint(0,con1)
+        tot += conr1
+        print("TOTALLL: ", tot)
+        con1 -= conr1
+        
         verde2.config(text="Semaforo2")
+        conr2 = random.randint(0,con2)
+        tot += conr2
+        print("TOTALLL: ", tot)
+        con2 -= conr2
         
         rojo1.config(text="Semaforo3")
         rojo2.config(text="")
@@ -60,33 +82,43 @@ def imprime(sem):
         ama1.config(text="")
         ama2.config(text="")
         
-        con = random.randint(0,con1) #Variable aleatoria 
-        con1 -= con
-        print(" Pasaron ",con," carros de los que habia en con1")
-        carros1.config(text=str(con1))
-        carros_pas1.config(text=str(con))
         
-        con = random.randint(0,con2)
-        con2 -= con
-        print(" Pasaron ",con," carros de los que habia en con2")
-        carros2.config(text=str(con2))
-        carros_pas2.config(text=str(con))
+        """print(" Pasaron ",con," carros de los que habia en sem3")
+        carros3.config(text=str(con3))
+        carros_pas3.config(text=str(con))"""
+        
         
     elif sem == "vuel":
         
         verde1.config(text="Semaforo1")
+        conr1 = random.randint(0,con1)
+        tot += conr1
+        con1 -= con
+        
         verde2.config(text="")
         
         rojo1.config(text="Semaforo2")
         rojo2.config(text="Semaforo3")
-        
+
         ama1.config(text="")
         ama2.config(text="")
         
-    elif sem == "v2":
+        print(" Pasaron ",conr," carros de los que habia en sem3")
+        carros3.config(text=str(con3))
+        carros_pas3.config(text=str(conr))
+        total()
+        print("Total:", conr)
         
+    elif sem == "v2":
+        total()
         verde1.config(text="Semaforo3")
         verde2.config(text="")
+        
+        conr = 0;
+        conr = random.randint(0,con3) #Variable aleatoria
+        tot += conr
+        print("TOTALLL: ", tot)
+        con3 -= conr
         
         rojo1.config(text="Semaforo1")
         rojo2.config(text="Semaforo2")
@@ -94,11 +126,15 @@ def imprime(sem):
         ama1.config(text="")
         ama2.config(text="")
         
-        con = random.randint(0,con3)
-        con3 -= con
-        print(" Pasaron ",con," carros de los que habia en con3")
-        carros3.config(text=str(con3))
-        carros_pas3.config(text=str(con))
+        
+        print(" Pasaron ",conr1," carros de los que habia en sem1")
+        carros1.config(text=str(con1))
+        carros_pas1.config(text=str(conr1))
+        
+        
+        print(" Pasaron ",conr2," carros de los que habia en sem2")
+        carros2.config(text=str(con2))
+        carros_pas2.config(text=str(conr2))
     
     elif sem == "a2":
         verde1.config(text="")
@@ -116,24 +152,26 @@ def imprime(sem):
         
     elif sem == "c1":
         
-        con = random.randint(1,5)
-        con1 += con
-        print(" Random:",con)
+        #con = random.randint(1,5)
+        con1 += 1
+        #print(" Random:",con1)
         carros1.config(text=str(con1))
         
     elif sem == "c2":
         
-        con = random.randint(1,5)
-        con2 += con
-        print("  Random:",con2)
+        #con = random.randint(1,5)
+        con2 += 1
+        #print("  Random:",con2)
         carros2.config(text=str(con2))
         
     elif sem == "c3":
         
-        con = random.randint(1,5)
-        con3 += con
-        print("   Random:",con3)
+        #con = random.randint(1,5)
+        con3 += 1
+        #print("   Random:",con3)
         carros3.config(text=str(con3))
+    
+    #total()
     
 def cierra():
     arduino.close()
@@ -195,13 +233,13 @@ lab1 = Label(raiz, text="Semaforo 3:", border=1)
 lab1.place(x=250, y=185)
 
 
-carros1 = Label(raiz, text="5")
+carros1 = Label(raiz, text=str(con1))
 carros1.place(x=120, y=145)
 
-carros2 = Label(raiz, text="5")
+carros2 = Label(raiz, text=str(con2))
 carros2.place(x=120, y=165)
 
-carros3 = Label(raiz, text="5")
+carros3 = Label(raiz, text=str(con3))
 carros3.place(x=120, y=185)
 
 
@@ -213,6 +251,13 @@ carros_pas2.place(x=340, y=165)
 
 carros_pas3 = Label(raiz, text="0")
 carros_pas3.place(x=340, y=185)
+
+
+lab1 = Label(raiz, text="Total de carros:", border=1)
+lab1.place(x=415, y=185)
+
+tot_pas = Label(raiz, text="0")
+tot_pas.place(x=530, y=185)
 
 
 #Etiquetas de los semaforos que cambian
